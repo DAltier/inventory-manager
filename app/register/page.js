743e367c6bf5e101ecaa967f33c97e3be5886f9c
DAltier <altierdana@gmail.com>
2024-08-06 15:42:43 -0700

    fixed signout

--- app/register/page.js
+++ app/register/page.js
@@ -2,28 +2,11 @@
 
 import React, { useState } from 'react';
 import { Container, TextField, Button, Typography, Box } from '@mui/material';
-import { styled } from '@mui/system';
 import { auth } from '../../firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
 import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
-
-const FormContainer = styled(Box)(() => ({
-  width: '100%',
-  maxWidth: '400px',
-  padding: '24px',
-  borderRadius: '20px',
-  backgroundColor: '#fff',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-}));
-
-const StyledTextField = styled(TextField)(() => ({
-  marginBottom: '20px',
-  width: '100%',
-}));
 
 const Register = () => {
   const [email, setEmail] = useState('');
